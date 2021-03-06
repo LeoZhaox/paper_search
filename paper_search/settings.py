52f505fc96536d983608f29b0e45f5b256dea8d9
icecola <fjl2401@163.com>
2021-03-07 01:09:13 +0800

    fix table

--- paper_search/settings.py
+++ paper_search/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'paper',
         'USER': 'root',
-        'PASSWORD': 'newpass',
+        'PASSWORD': 'ed2021',
         'HOST': 'localhost',
         'PORT': '3306',
     }
