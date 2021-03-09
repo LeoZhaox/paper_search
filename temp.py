e35072f7680aad2af142444e10fc21d0e54e744c
leo <leo>
2021-03-09 15:30:02 +0000

    add temp

---
+++ temp.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+"""
+@time: 3/9/21 1:14 PM
+@author: colaplusice
+@contact: fjl2401@163.com vx:18392358995
+"""
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paper_search.settings')
+django.setup()
+from paper.models import QuerySearch, Paper
+
+qs = QuerySearch.objects.all()
+
+for q in qs:
+    papers = q.papers
+    for p in papers:
+        exitst = Paper.objects.filter(id=p).exists()
+        if exitst:
+            print('exitst', q.word,q.papers)
